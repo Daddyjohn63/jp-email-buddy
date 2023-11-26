e8029fd110400b7775eb54fbb96b2276c98f37ec
Daddyjohn63 <john@webandprosper.co.uk>
2023-11-26 11:57:21 +0000

    new reqd

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -282,9 +282,7 @@ export default function ChatPage({ chatId, title, messages = [] }) {
                   <textarea
                     value={bookTitle}
                     onChange={(e) => setBookTitle(e.target.value)}
-                    placeholder={
-                      generatingResponse ? "" : "Book title: required."
-                    }
+                    placeholder={generatingResponse ? "" : "Book title..."}
                     className="w-full resize-none rounded-md bg-gray-700 p-2 text-white focus:border-emerald-500 focus:bg-gray-600 focus:outline focus:outline-emerald-500"
                   />
                   <textarea
